a3a4c9bec97036e686481330c2d26e7edb430b34
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2021-11-25 21:10:49 +0900

    added missed `is` assertion

--- test/rip/deadcode/computer/component/computer_test.clj
+++ test/rip/deadcode/computer/component/computer_test.clj
@@ -6,17 +6,17 @@
 
 (deftest computer-test
   (testing "computer"
-    (=
-      "a"
-      (with-out-str
-        (computer
-          (make-program-memory
-            [
-             (a-inst (bit15 (s2ba "a")))
-             (c-inst false op-a op-dest-d op-no-jump)       ; D=A
-             (a-inst console-idx)                           ; @console
-             (c-inst false op-d op-dest-m op-no-jump)       ; M=D
-             (a-inst (bit15 (i2ba 5)))                      ; @5
-             exit-inst                                      ; EXIT
-             (a-inst zero15)                                ; null
-             ]))))))
+    (is (=
+          "a"
+          (with-out-str
+            (computer
+              (make-program-memory
+                [
+                 (a-inst (bit15 (s2ba "a")))
+                 (c-inst false op-a op-dest-d op-no-jump)   ; D=A
+                 (a-inst console-idx)                       ; @console
+                 (c-inst false op-d op-dest-m op-no-jump)   ; M=D
+                 (a-inst (bit15 (i2ba 5)))                  ; @5
+                 exit-inst                                  ; EXIT
+                 (a-inst zero15)                            ; null
+                 ])))))))
